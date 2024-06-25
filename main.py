5753f96372a22425aec09d7d7cdba6123b31f206
StarkBak <116805997+SSTechInd-Tec@users.noreply.github.com>
2024-06-25 07:37:08 +0430

    game play logic finished

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+
 #  our game table 
 
 table = [
@@ -16,7 +18,10 @@ def print_table(table):
 
 
 def table_marker(table, mark, index):
-    table[index-1] = mark
+    if table[index-1] == ' ':
+        table[index-1] = mark
+    else:
+        print("This Cell Already Marked!")
 
 
 def check_winner(table):
@@ -46,8 +51,39 @@ def check_winner(table):
     ):
         return 'O is winner'
     else:
-        return 'No one is winner'
+        empty_cells_list = []
+        for cell in table:
+            if cell == ' ':
+                empty_cells_list.append(cell)
+        
+        if len(empty_cells_list) == 0:
+            return 'No one is winner'
+                
 
 # Game Play Logic
 
-print_table(table)
+while True:
+    x_index = int(input("Enter Your Position X (1-9): "))
+    table_marker(table2, 'X', x_index)
+    os.system('cls')
+    print_table(table2)
+    result_x = check_winner(table2)
+
+    if result_x == 'X is winner':
+        print(result_x)
+        break
+    elif result_x == 'No one is winner':
+        print(result_x)
+        break
+    
+    o_index = int(input("Enter Your Position O (1-9): "))
+    table_marker(table2, 'O', o_index)
+    os.system('cls')
+    print_table(table2)
+    result_o = check_winner(table2)
+    if result_o == 'O is winner':
+        print(result_o)
+        break
+    elif result_x == 'No one is winner':
+        print(result_x)
+        break
